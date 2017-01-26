41a75b32c23d5e771d796b1eda613d82e3dea1c3
James Billard <jamesbillard12@hotmail.com>
2017-01-26 00:57:00 -0800

    Button issues

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var userName = prompt('What\'s your name?');
 alert('Hello, ' + userName + '. Thanks for coming to my site!');
 
@@ -5,7 +6,7 @@ var answerOne = prompt('Was I born in Newfoundland?').toUpperCase();
 var myAnswerOne = 'YES';
 
 if (answerOne === myAnswerOne) {
-  alert('Very well done!')
+    alert('Very well done!')
 } else {
-  alert('Wrong! On to the next question!')
+    alert('Wrong! On to the next question!')
 }
