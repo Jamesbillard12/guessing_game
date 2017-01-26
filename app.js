e8e8e96861b76d553d26cc7b9f4c553ac2e52faf
James Billard <jamesbillard12@hotmail.com>
2017-01-26 01:14:34 -0800

    Help

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+< script type = 'text/javascript' >
+    function testEvent() {
+        var userName = prompt('What\'s your name?');
+        alert('Hello, ' + userName + '. Thanks for coming to my site!');
 
-var userName = prompt('What\'s your name?');
-alert('Hello, ' + userName + '. Thanks for coming to my site!');
+        var answerOne = prompt('Was I born in Newfoundland?').toUpperCase();
+        var myAnswerOne = 'YES';
 
-var answerOne = prompt('Was I born in Newfoundland?').toUpperCase();
-var myAnswerOne = 'YES';
-
-if (answerOne === myAnswerOne) {
-    alert('Very well done!')
-} else {
-    alert('Wrong! On to the next question!')
-}
+        if (answerOne === myAnswerOne) {
+            alert('Very well done!')
+        } else {
+            alert('Wrong! On to the next question!')
+        } <
+        /script>
+    }
