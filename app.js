f85fb09ad1dbf8cb0aa15f4ea1ff9f6815f25971
James Billard <jamesbillard12@hotmail.com>
2017-01-26 16:49:21 -0800

    Finished without button js loads first tho

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-<SCRIPT LANGUAGE="JavaScript">
-    function testEvent() {
-      var userName = prompt('What\'s your name?');
-      alert('Hello, ' + userName + '. Thanks for coming to my site!');
+var userName = prompt('What\'s your name?');
+alert('Hello, ' + userName + '. Thanks for coming to my site!');
 
-      var answerOne = prompt('Was I born in Newfoundland?').toUpperCase();
-      var myAnswerOne = 'YES';
+var answerOne = prompt('Was I born in Newfoundland?').toUpperCase();
+answerOne = answerOne.charAt(0)
+var myAnswerOne = 'Y';
 
-      if (answerOne === myAnswerOne) {
-          alert('Very well done!')
-      } else {
-          alert('Wrong! On to the next question!')
-      }
-    }
-</SCRIPT>
+if (answerOne === myAnswerOne) {
+  alert('Very well done!')
+}
+else {
+  alert('Wrong! On to the next question!')
+}
+
+var answerTwo = prompt('Did I go to university on Newfoundland?').toUpperCase();
+answerOne = answerOne.charAt(0)
+var myAnswerOne = 'N';
+
+if (answerOne === myAnswerOne) {
+  alert('Very well done!')
+}
+else {
+  alert('Wrong! On to the next question!')
+}
+
+var answerThree = prompt('Do I speak French?').toUpperCase();
+answerOne = answerOne.charAt(0)
+var myAnswerOne = 'Y';
+
+if (answerOne === myAnswerOne) {
+  alert('Very well done!')
+}
+else {
+  alert('Wrong! On to the next question!')
+}
+
+var answerThree = prompt('Have I lived in the Yukon Territory?').toUpperCase();
+answerOne = answerOne.charAt(0)
+var myAnswerOne = 'Y';
+
+if (answerOne === myAnswerOne) {
+  alert('Very well done!')
+}
+else {
+  alert('Wrong! On to the next question!')
+}
