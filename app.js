9c535e7cc8b7bfddf3efeadb988bbc6d660331bc
James Billard <jamesbillard12@hotmail.com>
2017-01-26 17:52:19 -0800

    Changed some js still getting Parsing error

--- app.js
+++ app.js
@@ -2,45 +2,45 @@ var userName = prompt('What\'s your name?');
 alert('Hello, ' + userName + '. Thanks for coming to my site!');
 
 var answerOne = prompt('Was I born in Newfoundland?').toUpperCase();
-answerOne = answerOne.charAt(0)
+answerOne = answerOne.charAt(0);
 var myAnswerOne = 'Y';
 
-if (answerOne === myAnswerOne) {
+if (answerOne === myAnswerOne); {
   alert('Very well done!')
 }
 else {
-  alert('Wrong! On to the next question!')
+  alert('Wrong! On to the next question!');
 }
 
 var answerTwo = prompt('Did I go to university on Newfoundland?').toUpperCase();
-answerTwo = answerTwo.charAt(0)
+answerTwo = answerTwo.charAt(0);
 var myAnswerTwo = 'N';
 
 if (answerTwo === myAnswerTwo) {
-  alert('Very well done!')
+  alert('Very well done!');
 }
 else {
-  alert('Wrong! On to the next question!')
+  alert('Wrong! On to the next question!');
 }
 
 var answerThree = prompt('Do I speak French?').toUpperCase();
-answerThree = answerThree.charAt(0)
+answerThree = answerThree.charAt(0);
 var myAnswerThree = 'Y';
 
 if (answerThree === myAnswerThree) {
-  alert('Very well done!')
+  alert('Very well done!');
 }
 else {
-  alert('Wrong! On to the next question!')
+  alert('Wrong! On to the next question!');
 }
 
 var answerFour = prompt('Have I lived in the Yukon Territory?').toUpperCase();
-answerFour = answerFour.charAt(0)
+answerFour = answerFour.charAt(0);
 var myAnswerFour = 'Y';
 
 if (answerFour === myAnswerFour) {
-  alert('Very well done!')
+  alert('Very well done!');
 }
 else {
-  alert('Wrong! On to the next question!')
+  alert('Wrong! On to the next question!');
 }
