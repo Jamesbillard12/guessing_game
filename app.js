b07297e222482ec17205fd48d279c3fad6cdec22
James Billard <jamesbillard12@hotmail.com>
2017-01-26 19:34:00 -0800

    FINALLY GOT BUTTON

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-var userName = prompt('What\'s your name?');
-alert('Hello, ' + userName + '. Thanks for coming to my site!');
+function testEvent(){
+  var userName = prompt('What\'s your name?');
+  alert('Hello, ' + userName + '. Thanks for coming to my site!');
 
-var answerOne = prompt('Was I born in Newfoundland?').toUpperCase();
-answerOne = answerOne.charAt(0);
-var myAnswerOne = 'Y';
+  var answerOne = prompt('Was I born in Newfoundland?');
+  answerOne = answerOne.charAt(0);
+  answerOne = answerOne.toUpperCase();
+  var myAnswerOne = 'Y';
 
-if (answerOne === myAnswerOne); {
-  alert('Very well done!')
-}
-else {
-  alert('Wrong! On to the next question!');
-}
+  if (answerOne === myAnswerOne) {
+    alert('Very well done!')
+  } else { //<=tSomething is happening here
+  };
 
-var answerTwo = prompt('Did I go to university on Newfoundland?').toUpperCase();
-answerTwo = answerTwo.charAt(0);
-var myAnswerTwo = 'N';
+  var answerTwo = prompt('Did I go to university on Newfoundland?');
+  answerTwo = answerTwo.charAt(0);
+  answerTwo = answerTwo.toUpperCase();
+  var myAnswerTwo = 'N';
 
-if (answerTwo === myAnswerTwo) {
-  alert('Very well done!');
-}
-else {
-  alert('Wrong! On to the next question!');
-}
+  if (answerTwo === myAnswerTwo) {
+    alert('Very well done!');
+  } else {
+    alert('Wrong! On to the next question!');
+  };
 
-var answerThree = prompt('Do I speak French?').toUpperCase();
-answerThree = answerThree.charAt(0);
-var myAnswerThree = 'Y';
+  var answerThree = prompt('Do I speak French?');
+  answerThree = answerThree.charAt(0);
+  answerThree = answerThree.toUpperCase();
+  var myAnswerThree = 'Y';
 
-if (answerThree === myAnswerThree) {
-  alert('Very well done!');
-}
-else {
-  alert('Wrong! On to the next question!');
-}
+  if (answerThree === myAnswerThree) {
+    alert('Very well done!');
+  } else {
+    alert('Wrong! On to the next question!');
+  };
 
-var answerFour = prompt('Have I lived in the Yukon Territory?').toUpperCase();
-answerFour = answerFour.charAt(0);
-var myAnswerFour = 'Y';
+  var answerFour = prompt('Have I lived in the Yukon Territory?');
+  answerFour = answerFour.charAt(0);
+  answerFour = answerFour.toUpperCase();
+  var myAnswerFour = 'Y';
 
-if (answerFour === myAnswerFour) {
-  alert('Very well done!');
-}
-else {
-  alert('Wrong! On to the next question!');
+  if (answerFour === myAnswerFour) {
+    alert('Very well done!');
+  } else {
+    alert('Wrong! On to the next question!');
+  };
 }
